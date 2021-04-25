b7c77f7963ad545050ec6b4ce96f07b977a96552
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-04-25 11:51:41 +0300

    .

--- src/IncomeExpensesContainer.tsx
+++ src/IncomeExpensesContainer.tsx
@@ -1,9 +1,13 @@
 import InputItem from './InputItem';
 
 function IncomeExpensesContainer(props: any) {
+    const onInputFunc = () => {
+        console.log('works');
+    }
+
     return (
     <div className={props.className}>
-        <InputItem title="Income" id="income" className="income" type="text" placeholder="Your income" />
+        <InputItem onClick={onInputFunc} title="Income" id="income" className="income" type="text" placeholder="Your income" />
         <div>
             <h3>Expenses</h3>
             <div id="expensesTotal">0</div>
