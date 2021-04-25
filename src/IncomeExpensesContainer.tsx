6f18c6c477714b2cdf09ac22bc95b2b6b8064c20
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-04-25 18:47:22 +0300

    .

--- src/IncomeExpensesContainer.tsx
+++ src/IncomeExpensesContainer.tsx
@@ -4,16 +4,21 @@ import InputItem from './InputItem';
 export default class IncomeExpensesContainer extends Component {
     state = {
         balance: 0,
-        incomeEntered: 0
+        incomeEntered: 0,
+        class: ''
     };
 
-    showCurrentBalance = (e: any): number => {
-        this.setState({ incomeEntered : e.target.value });
+    showCurrentBalance = (e: any): void => {
+        this.setState({ 
+            incomeEntered : e.target.value,
+            class: 'has-value' 
+        });
 
         console.log('entered value ', e.target.value );
         console.log('state value', this.state.incomeEntered);
 
-        return 1;
+        //incomeElement.classList.add("has-value");
+        console.log('state class ', this.state.class);
     };
     // const showCurrentBalance = () => {
     //     let balance: number = 0;
@@ -30,7 +35,7 @@ export default class IncomeExpensesContainer extends Component {
 
         return (
         <div className="income-expenses-container">
-            <InputItem onChange={this.showCurrentBalance} value={state.incomeEntered} title="Income" id="income" className="income" type="text" placeholder="Your income" />
+            <InputItem onChange={this.showCurrentBalance} value={state.incomeEntered}className="income" title="Income" id="income" type="text" placeholder="Your income" />
             <div>
                 <h3>Expenses</h3>
                 <div id="expensesTotal">0</div>
