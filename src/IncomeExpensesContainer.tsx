43f4b5838e05bba2e37294030b592fd7d367f4c2
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-04-25 18:54:35 +0300

    .

--- src/IncomeExpensesContainer.tsx
+++ src/IncomeExpensesContainer.tsx
@@ -3,25 +3,31 @@ import InputItem from './InputItem';
 
 export default class IncomeExpensesContainer extends Component {
     state = {
-        balance: 0,
         incomeEntered: 0,
-        class: ''
+        class: '',
+        expensesTotal: 0
     };
 
-    showCurrentBalance = (e: any): void => {
+    updateState = (e: any): void => {
         this.setState({ 
             incomeEntered : e.target.value,
             class: 'has-value' 
         });
 
         console.log('entered value ', e.target.value );
-        console.log('state value', this.state.incomeEntered);
 
         //incomeElement.classList.add("has-value");
-        console.log('state class ', this.state.class);
+        //console.log('state class ', this.state.class);
+
+        this.showCurrentBalance();
     };
-    // const showCurrentBalance = () => {
-    //     let balance: number = 0;
+
+    showCurrentBalance = (): void => {
+        console.log('aaaa ');
+
+        console.log('state value', this.state.incomeEntered);
+
+        //let balance: number = 0;
         //const incomeEntered: number = +incomeElement.value;
 
         // balance = incomeEntered - totalExpenses;
@@ -29,16 +35,17 @@ export default class IncomeExpensesContainer extends Component {
         // balanceElement.innerText = "" + balance;
         // incomeElement.classList.add("has-value");
         // balanceContainerElement.classList.add("visible");
-        // }  
+    };
+ 
     render() {
         const { state } = this;
 
         return (
         <div className="income-expenses-container">
-            <InputItem onChange={this.showCurrentBalance} value={state.incomeEntered}className="income" title="Income" id="income" type="text" placeholder="Your income" />
+            <InputItem onChange={this.updateState} value={state.incomeEntered} className="income" title="Income" id="income" type="text" placeholder="Your income" />
             <div>
                 <h3>Expenses</h3>
-                <div id="expensesTotal">0</div>
+                <div id="expensesTotal">{state.expensesTotal}</div>
             </div> 
         </div>
         );
